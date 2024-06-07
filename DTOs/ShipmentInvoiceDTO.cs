018bd0f5233c21350a81c72ed393c211e1dc40d0
Daria Kyselytsia <daria.kys1@gmail.com>
2024-06-08 00:27:24 +0300

    Changes

--- DTOs/ShipmentInvoiceDTO.cs
+++ DTOs/ShipmentInvoiceDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTOs
 {
     public class ShipmentInvoiceDTO
     {
         public int Id { get; set; }
         public string InvoiceNumber { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Total amount must be greater than 0")]
         public decimal? TotalAmount { get; set; }
         public string StoreName { get; set; }
         public string EmployeeName { get; set; }
--- DTOs/SupplyDetailsDTO.cs
+++ DTOs/SupplyDetailsDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTOs
 {
     public class SupplyDetailsDTO
     {
         public int SupplyDetailsId { get; set; }
         public string ProductName { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Amount must be greater than 0")]
         public int? Amount { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price per unit must be greater than 0")]
         public decimal? PricePerUnit { get; set; }
         public List<ShipmentInvoiceDTO> ShipmentInvoices { get; set; } = new List<ShipmentInvoiceDTO>();
     }
